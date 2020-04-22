612234efc6fd5783da0a4225c9cc2635a8fd1b1e
xmoutaz <xMoutaz@hotmail.com>
2020-04-22 12:11:03 +0000

    adding translation labels

--- src/app/admin-home/components/add-word/add-word.component.ts
+++ src/app/admin-home/components/add-word/add-word.component.ts
@@ -1,28 +1,27 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
-import { TranslationsMdbService } from 'src/app/shared/services/Mongodb/translations-mdb.service';
-import { NewWord } from 'src/app/shared/models/newWord';
-import { Language } from 'src/app/shared/models/language';
-import { LanguagesService } from 'src/app/shared/services/languages.service';
-import {  Observable, Subscription } from 'rxjs';
-import { AppState } from 'src/app/state/models/app-state-models';
-import { Store } from '@ngrx/store';
 import { Location } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
+import { Language } from 'src/app/shared/models/language';
+import { NewWord } from 'src/app/shared/models/newWord';
+import { TranslationsMdbService } from 'src/app/shared/services/Mongodb/translations-mdb.service';
+import { AppState } from 'src/app/state/models/app-state-models';
 
 @Component({
   selector: 'app-add-word',
   templateUrl: './add-word.component.html',
   styleUrls: ['./add-word.component.css']
 })
-export class AddWordComponent implements OnInit, OnDestroy {
+export class AddWordComponent implements OnInit {
   newWord: NewWord = <any>{};
   language = new Language();
   languages$: Observable<Array<any>>;
-  subscription: Subscription;
   
   constructor(private store: Store<AppState>, private translationServiceMdb: TranslationsMdbService, 
     private _location: Location, private router: Router) {
-    this.store.select(store => store)
+    this.store.select(store => store).pipe(take(1))
       .subscribe(state => {
         this.language.language = state.routerReducer.state.root.firstChild.firstChild.params.lang;
         console.log(this.language);
@@ -32,7 +31,7 @@ export class AddWordComponent implements OnInit, OnDestroy {
   ngOnInit() {
     this.newWord.word = {};
     this.languages$ = this.store.select(store => store.language.list);
-    this.subscription =  this.store.select(store => store.language.list)
+      this.store.select(store => store.language.list)
       .subscribe((data: any) => {
         this.languages$ = data;
         data.forEach(data => {
@@ -52,7 +51,4 @@ export class AddWordComponent implements OnInit, OnDestroy {
     this._location.back();
   }
 
-  ngOnDestroy() {
-    this.subscription.unsubscribe();
-  }
 }
