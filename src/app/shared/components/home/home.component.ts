618e507f0ddbb9d805b012d5e414283ca08d6137
xmoutaz <xMoutaz@hotmail.com>
2020-04-22 17:11:21 +0000

    editing weather in home page

--- src/app/shared/components/home/home.component.ts
+++ src/app/shared/components/home/home.component.ts
@@ -9,52 +9,20 @@ import { filter } from 'rxjs/operators';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  
   public weatherSearchForm: FormGroup;
   public weatherData: any;
-  public lat;
-  public lng;
 
-  constructor(private formBuilder: FormBuilder, private apixuService: ApixuService) { }
+  constructor(private apixuService: ApixuService) { }
 
   ngOnInit() {
-    this.getLocation();
-    this.weatherSearchForm = this.formBuilder.group({
-      location: ['']
-    });
+    this.sendToAPIXU('Tetouan');
   }
 
-  sendToAPIXU(formValues) {
-    this.apixuService
-      .getWeather(formValues.location)
-      .pipe(filter((data:any) => !data.success))
-      .subscribe(data => {this.weatherData = data; console.log(data)});
-  }
-  sendLngLatToAPIXU(location) {
+  sendToAPIXU(location) {
     this.apixuService
       .getWeather(location)
-      .pipe(filter((data:any) => !data.success))
       .subscribe(data => {this.weatherData = data; console.log(data)});
   }
 
-  getLocation() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position: Position) => {
-        if (position) {
-          console.log("Latitude: " + position.coords.latitude +
-            "Longitude: " + position.coords.longitude);
-          this.lat = position.coords.latitude;
-          this.lng = position.coords.longitude;
-          console.log(this.lat);
-          console.log(this.lng);
-          let location = `${this.lat}, ${this.lng}`;
-          this.sendLngLatToAPIXU(location);
-        }
-      },
-        (error: PositionError) => console.log(error));
-    } else {
-      alert("Geolocation is not supported by this browser.");
-    }
-  }
 
 }
