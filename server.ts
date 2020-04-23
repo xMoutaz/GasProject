c84f3c6d905fb6afce3aaed728e5a825803b5dcb
xmoutaz <xMoutaz@hotmail.com>
2020-04-23 13:02:37 +0000

    heroku configurations

--- server.ts
+++ server.ts
@@ -4,12 +4,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/gas-project'));
+app.use(express.static(__dirname + '/dist/GasProject'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/gas-project/index.html'));
+res.sendFile(path.join(__dirname+'/dist/GasProject/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080, () => console.log('Server started at port: '+ 8081));
