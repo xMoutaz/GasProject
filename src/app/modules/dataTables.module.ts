7f589bb2b1e74f2370246817a078b9d7ce5d28b9
xmoutaz <xMoutaz@hotmail.com>
2020-04-23 15:37:43 +0000

    testing

--- src/app/modules/dataTables.module.ts
+++ src/app/modules/dataTables.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { DataTableComponent } from 'src/app/components/controls/data-table/data-table.component';
 
-
 @NgModule({
   declarations: [DataTableComponent],
   exports:[
