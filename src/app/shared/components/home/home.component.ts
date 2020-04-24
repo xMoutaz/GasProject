1eb373d5585d2d30ab17b62edb9980b4bdcacf79
xmoutaz <xMoutaz@hotmail.com>
2020-04-24 11:25:49 +0000

    adding user name into the home page

--- src/app/shared/components/home/home.component.ts
+++ src/app/shared/components/home/home.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { ApixuService } from '../services/apixu.service';
 import { filter } from 'rxjs/operators';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/state/models/app-state-models';
 
 @Component({
   selector: 'app-home',
@@ -11,17 +13,23 @@ import { filter } from 'rxjs/operators';
 export class HomeComponent implements OnInit {
   public weatherSearchForm: FormGroup;
   public weatherData: any;
+  appUser: any;
 
-  constructor(private apixuService: ApixuService) { }
+  constructor(private apixuService: ApixuService, private store: Store<AppState>) { }
 
   ngOnInit() {
-    this.sendToAPIXU('Tetouan');
+    this.sendToAPIXU();
     // TODO: -Access user from store here and display user name in welcome message
+    this.store.select(store => store.User.user)
+      .pipe(filter((data) => !!data))
+      .subscribe(data => {
+        this.appUser = data;
+      });
   }
 
-  sendToAPIXU(location) {
+  sendToAPIXU() {
     this.apixuService
-      .getWeather(location)
+      .getWeather()
       .subscribe(data => {this.weatherData = data;});
   }
 
